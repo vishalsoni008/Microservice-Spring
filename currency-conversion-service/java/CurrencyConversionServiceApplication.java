33b5a6fbbc18b210c0d6901ef9c7bf5072cc5152
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2020-12-15 15:04:02 +0530

    Add files via upload

---
+++ currency-conversion-service/java/CurrencyConversionServiceApplication.java
@@ -0,0 +1,17 @@
+package com.vishal.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.vishal.microservices.currencyconversionservice")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-service/java/CurrencyExchangeProxy.java
@@ -0,0 +1,19 @@
+package com.vishal.microservices.currencyconversionservice;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.vishal.microservices.currencyconversionservice.Bean.CurrencyConversionBean;
+
+//@FeignClient(name = "currency-exchange-service",url = "localhost:8001")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	  public CurrencyConversionBean retriveExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to);
+
+}
